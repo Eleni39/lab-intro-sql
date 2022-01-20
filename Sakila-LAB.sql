64fe23812e8acde50c56904b217f228c7ef656bd
Eleni39 <eleni.g06@gmail.com>
2022-01-20 16:33:54 +0100

    first commit

---
+++ Sakila-LAB.sql
@@ -0,0 +1,16 @@
+USE sakila;
+-- Question number 2 - Get data from tables actor, film and customer
+SELECT * from actor;
+SELECT * from film;
+SELECT * from customer;
+-- Question number 3 
+SELECT title from film;
+-- Question number 4
+SELECT name AS language FROM language;
+-- Question number 5
+SELECT count(*)FROM store;
+SELECT count(*) FROM staff;
+SELECT first_name from staff;
+
+
+
